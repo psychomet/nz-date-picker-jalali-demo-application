00617841923fb9ddf654bae52d545496d0e8909d
psychomet <sanatkar.dev@gmail.com>
2022-12-19 12:37:35 +0330

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nz-date-picker-jalali-demo-application-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'nz-date-picker-jalali-demo-application';
+}
